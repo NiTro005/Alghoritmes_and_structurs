83101e42573794c83828f1a19a5f5e21962678df
NiTro005 <nitro2005002@gmail.com>
2024-09-09 13:47:39 +0300

    deleted non member functions

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,15 @@
 // Copyright 2024 Marina Usova
 
-#define EASY_EXAMPLE
+#define PAIR
 #ifdef EASY_EXAMPLE
-
 #include <iostream>
 #include <iomanip>
 #include "../lib_easy_example/easy_example.h"
 
-#include "../lib_pair/pair.h"
+
 
 int main() {
-#ifdef EASY_EXAMPLE
+
 
     int a, b;
     float result;
@@ -36,20 +35,22 @@ int main() {
     catch (std::exception err) {
         std::cerr << err.what() << std::endl;
     }
+    return 0;
+    }
 #endif  // EASY_EXAMPLE
-#ifdef Pair
-    TPair<int, int> pair1(1, 4);
-    TPair<int, int> pair2(10, 10);
+#ifdef PAIR
+#include "../lib_pair/pair.h"
+    int main() {
+        TPair<int, int> pair1(1, 4);
+        TPair<int, int> pair2(10, 10);
 
-    TPair<int, int> resultPair = pair1 + pair2;
-    std::cout << "Pair1 + Pair2 = " << resultPair << std::endl;
+        TPair<int, int> resultPair = pair1 + pair2;
+        std::cout << "Pair1 + Pair2 = " << resultPair << std::endl;
 
-    pair1.swap(pair2);
-    std::cout << "After swap: Pair1 = " << pair1 << ", Pair2 = " << pair2 << std::endl;
+        pair1.swap(pair2);
+        std::cout << "After swap: Pair1 = " << pair1 << ", Pair2 = " << pair2 << std::endl;
+        return 0;
+    }
 #endif // Pair
 
-
-    return 0;
-}
-#endif  // EASY_EXAMPLE
 
