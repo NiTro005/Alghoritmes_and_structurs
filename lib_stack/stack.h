3e8fc00008ee91f2ab93581b12c85b61b6542177
NiTro005 <nitro2005002@gmail.com>
2024-09-16 15:50:00 +0300

    push method and test for him

--- lib_stack/stack.h
+++ lib_stack/stack.h
@@ -8,33 +8,53 @@ class TStack {
 	TDMassive<T> _data;
  public:
 	 TStack(size_t size = 20);
-	// ~TStack();
+	 ~TStack();
 
-	 size_t size();
+	 inline size_t size() const noexcept;
 
-	// inline bool IsEmpty() const noexcept;
-	 //inline bool IsFool() const noexcept;
+	 inline bool IsEmpty() const noexcept;
+	 inline bool IsFull() const noexcept;
 
-	/// void push();
-	 ///void pop();
-	 ///inline T top() const noexcept;
+	 void push(T value);
+	// void pop();
+	 inline T top() const;
 };
 
 template <typename T>
 TStack<T>::TStack(size_t size) : _data(size) {}
 
 template <typename T>
-size_t TStack<T>::size() {
+inline size_t TStack<T>::size() const noexcept {
 	return _data.capacity();
 }
 
-//template <typename T>
-//TStack<T>::~TStack(){}
-//
-//inline bool IsEmpty() const noexcept {
-//	return _data->empty();
-//}
-//
-//inline bool IsFull() const noexcept {
-//	return _data->full();
-//}
+template <typename T>
+TStack<T>::~TStack(){}
+
+template <typename T>
+inline bool TStack<T>:: IsEmpty() const noexcept {
+	return _data.empty();
+}
+
+template <typename T>
+inline bool TStack<T>:: IsFull() const noexcept {
+	return _data.full();
+}
+
+template<typename T>
+void TStack<T>:: push(T value){
+	if (!IsFull()) {
+		_data.push_front(value);
+	} else {
+		throw std::logic_error("Error:Overlow stack");
+	}
+}
+
+template<typename T>
+inline T TStack<T>::top() const
+{
+	if (IsEmpty()) {
+		throw std::out_of_range("Stack is empty");
+	}
+	return _data.data()[0];
+}
--- tests/test_stack.cpp
+++ tests/test_stack.cpp
@@ -3,7 +3,19 @@
 #include "../lib_dmassive/archive.h"
 #include "../lib_stack/stack.h"
 
-TEST(Test_TStack, Constructor) {
+//TEST(Test_TStack, Constructor) {
+//	TStack<int> st1;
+//	EXPECT_EQ(st1.size(), 20);
+//}
+
+TEST(Test_TStack, empty_metod) {
 	TStack<int> st1;
-	EXPECT_EQ(st1.size(), 20);
+	EXPECT_TRUE(st1.IsEmpty());
+}
+
+TEST(Test_TStack, PUSH_metod) {
+	TStack<int> st1(1);
+	st1.push(1);
+	ASSERT_ANY_THROW(st1.push(2));
+	EXPECT_EQ(st1.top(), 1);
 }
