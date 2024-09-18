3a8b31a49961761f90970d43ae2f2d77b401d701
NiTro005 <nitro2005002@gmail.com>
2024-09-18 22:43:59 +0300

    ...

--- lib_vector/tvector.h
+++ lib_vector/tvector.h
@@ -2,3 +2,14 @@
 #pragma once
 #include <iostream>
 #include "../lib_dmassive/archive.h"
+
+template <typename T>
+class TVector {
+	TDMassive _data;
+	size_t _start_index;
+
+ public:
+	 TVector();
+	 TVector(const TVector& vec);
+	 TVector(size_t start_index);
+};
