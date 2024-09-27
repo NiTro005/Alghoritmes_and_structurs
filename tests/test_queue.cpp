affedfff48c29a302bbad7c85415346a2696a177
NiTro005 <nitro2005002@gmail.com>
2024-09-27 21:59:57 +0300

    added all tests for library Queue

--- tests/test_queue.cpp
+++ tests/test_queue.cpp
@@ -2,3 +2,50 @@
 #include <gtest.h>
 #include "../lib_dmassive/archive.h"
 #include "../lib_queue/queue.h"
+
+TEST(TQueueTest, ConstructorAndDestructor) {
+    TQueue<int> queue(10);
+    EXPECT_EQ(queue.size(), 10);
+    EXPECT_TRUE(queue.IsEmpty());
+    EXPECT_FALSE(queue.IsFull());
+}
+
+TEST(TQueueTest, Push) {
+    TQueue<int> queue(3);
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    EXPECT_FALSE(queue.IsEmpty());
+    EXPECT_TRUE(queue.IsFull());
+    EXPECT_EQ(queue.top(), 1);
+}
+
+TEST(TQueueTest, Pop) {
+    TQueue<int> queue(3);
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    queue.pop();
+    EXPECT_EQ(queue.top(), 2);
+    queue.pop();
+    EXPECT_EQ(queue.top(), 3);
+    queue.pop();
+    EXPECT_TRUE(queue.IsEmpty());
+}
+
+TEST(TQueueTest, PushOverflow) {
+    TQueue<int> queue(2);
+    queue.push(1);
+    queue.push(2);
+    EXPECT_THROW(queue.push(3), std::out_of_range);
+}
+
+TEST(TQueueTest, PopEmpty) {
+    TQueue<int> queue(2);
+    EXPECT_THROW(queue.pop(), std::out_of_range);
+}
+
+TEST(TQueueTest, TopEmpty) {
+    TQueue<int> queue(2);
+    EXPECT_THROW(queue.top(), std::out_of_range);
+}
