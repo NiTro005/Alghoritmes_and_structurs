688a3b24382a8f94c925838d76b61f3c04b3668b
NiTro005 <nitro2005002@gmail.com>
2024-10-08 22:27:29 +0300

    add all addition methods in TList

--- lib_list/tlist.h
+++ lib_list/tlist.h
@@ -9,8 +9,8 @@ class TList {
     TNode<T>* head;
     TNode<T>* last;
  public:
-     TList();
-     TList(const TNode& node);
+     TList() = default;
+     TList(const TList& list);
      ~TList();
 
      void push_front(const T& value) noexcept;
@@ -31,3 +31,74 @@ class TList {
 
      void asort(TList<T>& list) noexcept;
 };
+
+
+template<typename T>
+TList<T>::TList<T>(const TList<T>& list) : head(list.head), last(list.last) {}
+
+template<typename T>
+TList<T>::~TList(){
+    while (head != nullptr) {
+        Node<T>* link = head;
+        head = head->next();
+        delete link;
+    }
+}
+
+template<typename T>
+void TList<T>::push_front(const T& value) noexcept {
+    if (head == nullptr) {
+        head = new TNode<T>(value);
+        last = head;
+    } else {
+        TNode<T>* link = new TNode<T>(value, head);
+        head = link;
+    }
+}
+
+template<typename T>
+void TList<T>::push_back(const T& value) noexcept {
+    if (last == nullptr) {
+        head = new TNode<T>(value);
+        last = head;
+    } else {
+        TNode<T>* link = new TNode<T>(value);
+        last->next(link);
+        last = link;
+    }
+}
+
+template<typename T>
+void TList<T>::insert(TNode<T>* node, const T& value) {
+    if (node == nullptr) {
+        throw std::invalid_argument("Node pointer is nullptr");
+    }
+    if (node->next() == nullptr) {
+        TNode<T>* newNode = new TNode<T>(value);
+        node->next(newNode);
+        last = newNode;
+    } else {
+        TNode<T>* newNode = new TNode<T>(value, node->next());
+        node->next(newNode);
+    }
+}
+
+template<typename T>
+void TList<T>::insert(size_t pos, const T& value) {
+    TNode<T>* cur = head;
+    if (pos == 0) {
+        push_front(value);
+        return;
+    }
+    for (size_t i = 0; i != pos; i++) {
+        cur = cur->next();
+        if (cur == nullptr) { throw std::logic_error("Out of range"); }
+    }
+    if (cur->next() == nullptr) { 
+        push_back(value); 
+        return;
+    }
+    insert(cur, value);
+}
+
+
