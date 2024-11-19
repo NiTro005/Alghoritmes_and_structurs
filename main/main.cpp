5778689499194d9089eb8189162f56f39a2e001a
NiTro005 <nitro2005002@gmail.com>
2024-11-20 00:30:58 +0300

    ...

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,6 @@
 // Copyright 2024 Marina Usova
 
-#define DMASSIVE 
+#define LIST 
 #ifdef EASY_EXAMPLE
 #include <iostream>
 #include <iomanip>
@@ -54,50 +54,54 @@ int main() {
 #include <ctime>
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
 #include "../lib_list/tlist.h"
+#include <cstdint>
+#include <random>
+
     int main() {
-        srand(time(NULL));
-        long int size;
+        std::mt19937 gen(static_cast<unsigned int>(time(NULL)));
+        int64_t size;
         std::cout << "Enter the size of the array: ";
         std::cin >> size;
-        TList<long int> mas;
-        TList<long int> mas1;
-        TList<long int> mas2;
+        TList<int64_t> mas;
+        TList<int64_t> mas1;
+        TList<int64_t> mas2;
 
         double timespent[10];
         clock_t start = clock();
         for (size_t i = 0; i < size; i++) {
             mas.push_back(i);
         }
-        
+
         clock_t end = clock();
-        timespent[0] = (double)(end - start);
+        timespent[0] = static_cast<double>(end - start);
         std::cout << "Time spent push_back method in list: " << timespent[0] << " ms" << std::endl;
         start = clock();
-        mas.find(0 + rand() % size);
-        mas.find(0 + rand() % size);
-        mas.find(0 + rand() % size);
-        mas.find(0 + rand() % size);
-        mas.find(0 + rand() % size);
-        mas.find(0 + rand() % size);
-        mas.find(0 + rand() % size);
+        mas.find(0 + gen() % size);
+        mas.find(0 + gen() % size);
+        mas.find(0 + gen() % size);
+        mas.find(0 + gen() % size);
+        mas.find(0 + gen() % size);
+        mas.find(0 + gen() % size);
+        mas.find(0 + gen() % size);
         end = clock();
-        timespent[6] = (double)(end - start) / 7;
+        timespent[6] = static_cast<double>(end - start) / 7;
         std::cout << "Time spent find method in list: " << timespent[6] << " ms" << std::endl;
 
         start = clock();
         for (size_t i = 0; i < size; i++) {
-            mas1.push_front(0 + rand() % 1000);
+            mas1.push_front(0 + gen() % 1000);
         }
         end = clock();
-        timespent[1] = (double)(end - start);
+        timespent[1] = static_cast<double>(end - start);
 
         start = clock();
         for (size_t i = 0; i < size; i++) {
-            mas2.insert(i, 0 + rand() % 1000);
+            mas2.insert(i, 0 + gen() % 1000);
         }
         end = clock();
-        timespent[2] = (double)(end - start);
+        timespent[2] = static_cast<double>(end - start);
         std::cout << std::endl;
         std::cout << "Time spent push_front method in list: " << timespent[1] << " ms" << std::endl;
         std::cout << "Time spent insert method in list: " << timespent[2] << " ms" << std::endl;
@@ -107,14 +111,14 @@ int main() {
             mas.pop_back();
         }
         end = clock();
-        timespent[3] = (double)(end - start);
+        timespent[3] = static_cast<double>(end - start);
 
         start = clock();
         for (size_t i = 0; i < size; i++) {
             mas1.pop_front();
         }
         end = clock();
-        timespent[4] = (double)(end - start);
+        timespent[4] = static_cast<double>(end - start);
 
         for (auto it = mas.begin(); it != mas.end(); ++it) {
             std::cout << *it << " ";
@@ -125,24 +129,26 @@ int main() {
         std::cout << "Time spent pop_front method in list: " << timespent[4] << " ms" << std::endl;
 
         return 0;
-}
+    }
 #endif  // List
 
 #ifdef DMASSIVE
-#include "../lib_dmassive/archive.h"
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
 #include <ctime>
-#define FIND_VALUE 10000000
+#include <cstdint>
+#include <random>
+#include "../lib_dmassive/archive.h"
 
     int main() {
-        srand(time(NULL));
-        long int size;
+        std::mt19937 gen(static_cast<unsigned int>(time(NULL)));
+        int64_t size;
         std::cout << "Enter the size of the array: ";
         std::cin >> size;
-        TDMassive<long int> mas(size);
-        TDMassive<long int> mas1(size);
-        TDMassive<long int> mas2(size);
+        TDMassive<int64_t> mas(size);
+        TDMassive<int64_t> mas1(size);
+        TDMassive<int64_t> mas2(size);
 
         double timespent[10];
         clock_t start = clock();
@@ -150,68 +156,64 @@ int main() {
             mas.push_back(i);
         }
         clock_t end = clock();
-        timespent[0] = (double)(end - start);
+        timespent[0] = static_cast<double>(end - start);
         std::cout << "Time spent push_back method in massive: " << timespent[0] << " ms" << std::endl;
 
         start = clock();
-        mas.find_first(0 + rand() % size);
-        mas.find_first(0 + rand() % size);
-        mas.find_first(0 + rand() % size);
-        mas.find_first(0 + rand() % size);
-        mas.find_first(0 + rand() % size);
-        mas.find_first(0 + rand() % size);
-        mas.find_first(0 + rand() % size);
+        mas.find_first(0 + gen() % size);
+        mas.find_first(0 + gen() % size);
+        mas.find_first(0 + gen() % size);
+        mas.find_first(0 + gen() % size);
+        mas.find_first(0 + gen() % size);
+        mas.find_first(0 + gen() % size);
+        mas.find_first(0 + gen() % size);
         end = clock();
-        timespent[6] = (double)(end - start) / 6;
+        timespent[6] = static_cast<double>(end - start) / 6;
         std::cout << "Time spent find method in massive: " << timespent[6] << " ms" << std::endl;
-
 
         start = clock();
         for (size_t i = 0; i < size; i++) {
-            mas1.push_front(0 + rand() % 1000);
+            mas1.push_front(0 + gen() % 1000);
         }
         end = clock();
-        timespent[1] = (double)(end - start);
+        timespent[1] = static_cast<double>(end - start);
 
         std::cout << "Time spent push_front method in massive: " << timespent[1] << " ms" << std::endl;
 
         start = clock();
         for (size_t i = 0; i < size; i++) {
-            mas2.insert(0 + rand() % 1000, i);
+            mas2.insert(0 + gen() % 1000, i);
         }
         end = clock();
-        timespent[2] = (double)(end - start);
-        
-       
+        timespent[2] = static_cast<double>(end - start);
+
         std::cout << "Time spent insert method in massive: " << timespent[2] << " ms" << std::endl;
         std::cout << std::endl;
-
-       
 
         start = clock();
         for (size_t i = 0; i < size; i++) {
             mas.pop_back();
         }
         end = clock();
-        timespent[3] = (double)(end - start);
+        timespent[3] = static_cast<double>(end - start);
 
         start = clock();
         for (size_t i = 0; i < size; i++) {
             mas1.pop_front();
         }
         end = clock();
-        timespent[4] = (double)(end - start);
+        timespent[4] = static_cast<double>(end - start);
 
         start = clock();
         for (size_t i = 0; i < size; i++) {
             mas2.remove_by_index(i);
         }
         end = clock();
-        timespent[5] = (double)(end - start);
+        timespent[5] = static_cast<double>(end - start);
 
-            for (auto it = mas.begin(); it != mas.end(); ++it) {
-                std::cout << *it << " ";
-            }
+        for (auto it = mas.begin(); it != mas.end(); ++it) {
+            std::cout << *it << " ";
+        }
 
         std::cout << std::endl;
         std::cout << "Time spent pop_back method in massive: " << timespent[3] << " ms" << std::endl;
