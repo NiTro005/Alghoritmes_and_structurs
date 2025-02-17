d901223d06df89ddaa0c08f1bb8e34b2973bada7
NiTro005 <nitro2005002@gmail.com>
2025-02-17 09:51:24 +0300

    realise interface itable

---
+++ lib_itable/itable.h
@@ -0,0 +1,13 @@
+// Copyright 2024 Kita Trofimov
+#pragma once
+#include <iostream>
+template<class Tkey, class Tval>
+class ITable {
+ public:
+    virtual Tkey insert(TVal val);
+    virtual void insert(Tkey key, Tval val);
+    virtual void erase(TKey key);
+    virtual Tval find(Tkey key);
+    virtual operator[](Tkey key);
+    virtual operator=(const ITable<TKey, Tval>& > tab);
+};
