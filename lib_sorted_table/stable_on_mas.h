82085f7510a89fb8daa01c40152df0d508081b2f
NiTro005 <nitro2005002@gmail.com>
2025-02-28 19:03:52 +0300

    added constructor inicialization with sort

--- lib_sorted_table/stable_on_mas.h
+++ lib_sorted_table/stable_on_mas.h
@@ -6,7 +6,7 @@
 #include "../lib_itable/itable.h"
 
 template <class Tkey, class Tval>
-class SortedTabOnMas : ITable {
+class SortedTabOnMas : ITable<Tkey, Tval> {
     TDMassive<TPair<Tkey, Tval>> _data;
 
  public:
@@ -21,3 +21,20 @@ class SortedTabOnMas : ITable {
      Tval operator[](Tkey key) override;
      SortedTabOnMas& operator=(const SortedTabOnMas<Tkey, Tval>& tab);
 };
+
+template<class Tkey, class Tval>
+SortedTabOnMas<Tkey, Tval>::SortedTabOnMas
+(const TDMassive<TPair<Tkey, Tval>>& mas) {
+    TDMassive<TPair<Tkey, Tval>> new_mas(mas);
+    for (size_t i = 0; i < new_mas.size(); i++) {
+        for (size_t j = i; j > 0 && new_mas[j - 1].second() > new_mas[j].second(); j--) {
+            new_mas.swap(new_mas[j - 1], new_mas[j]);
+        }
+    }
+    _data = new_mas;
+}
+
+template<class Tkey, class Tval>
+inline SortedTabOnMas<Tkey, Tval>::SortedTabOnMas
+(const SortedTabOnMas<Tkey, Tval>& tab): _data(tab._data) {}
+
