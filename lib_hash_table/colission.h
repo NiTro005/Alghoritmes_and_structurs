9d9bd055d2a860e5cdcd44bca8e5d40453959a90
Kita Trofimov <144846094+NiTro005@users.noreply.github.com>
2025-04-05 14:41:55 +0300

    implemented the basic structure of the library

--- lib_hash_table/colission.h
+++ lib_hash_table/colission.h
@@ -4,3 +4,27 @@
 
 #include "../lib_list/tlist.h"
 #include "../lib_pair/pair.h"
+
+enum typeColission {
+    OpenAddress, Chain
+};
+
+template <class Tkey, class Tval>
+class IColission {
+ public:
+     virtual void insert(Tkey key, Tval val) = 0;
+     virtual void erase(Tkey key) = 0;
+     virtual Tval find(Tkey key) = 0;
+     virtual size_t search(Tkey key) = 0;
+     virtual Tval operator[](Tkey key) = 0;
+};
+
+template <class Tkey, class Tval>
+class OpenAddressColission : public IColission<Tkey, Tval>{
+
+};
+
+template <class Tkey, class Tval>
+class ChainStrategyColission : public IColission<Tkey, Tval> {
+
+};
